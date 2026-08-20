8ac0872979a9ba70f0be179010ad6bdb5c602e83
brayden20149-create <brayden20149@icloud.com>
2026-08-20 17:47:41 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import NutriLog from "./NutriLog.jsx";
+import App from "./App.jsx";
 
 const style = document.createElement("style");
-
 style.textContent = `
-  html,
-  body,
-  #root {
-    height: 100%;
-    margin: 0;
-    padding: 0;
-    background: #0b0f0b;
-  }
-
-  * {
-    box-sizing: border-box;
-    -webkit-tap-highlight-color: transparent;
-  }
-
-  body {
-    overscroll-behavior: none;
-  }
+  html, body, #root { height: 100%; margin: 0; padding: 0; background: #0b0f0b; }
+  * { -webkit-tap-highlight-color: transparent; }
 `;
-
 document.head.appendChild(style);
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NutriLog />
+    <App />
   </React.StrictMode>
 );
